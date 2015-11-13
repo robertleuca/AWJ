1f1082972b1cfd07a1f9741bf29fd88b061f8fe7
robertleuca <robert_daniel.leuca@aut.pub.ro>
2015-11-13 17:27:39 +0200

    Am adaugat tema 3

---
+++ spring-java-app/initial/src/main/java/hello/models/Masina.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Masina {
+  private String name;
+  private int id;
+
+  public Masina() {}
+
+  public Masina(int id, String name) {
+      this.name = name;
+      this.id = id;
+  }
+
+  public String getName() {
+      return this.name;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+  public void setName(String name){
+        this.name=name;
+    }
+}
+
---
+++ spring-java-app/initial/src/main/java/hello/models/Produs.java
@@ -0,0 +1,27 @@
+package hello;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Produs {
+  private String name;
+  private int id;
+
+  public Produs() {}
+
+  public Produs(int id, String name) {
+      this.name = name;
+      this.id = id;
+  }
+
+  public String getName() {
+      return this.name;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+  public void setName(String name){
+        this.name=name;
+    }
+}
